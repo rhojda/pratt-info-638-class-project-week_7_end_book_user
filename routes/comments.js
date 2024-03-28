899c9d1a5d4749d627f8aaee1be5a0aec22035e2
Rafaela <rhojda@pratt.edu>
2024-03-27 22:40:46 -0400

    Still with issues

--- routes/comments.js
+++ routes/comments.js
@@ -4,22 +4,24 @@ const Comment = require('../models/comment');
 
 
 router.get('/edit', async (req, res, next) => {
-    let bookId = req.query.id;
-    let author = Author.get(authorIndex);
-    res.render('books/form', { title: 'BookedIn || Authors', author: author, authorIndex: authorIndex });
+    let commentIndex = req.query.id;
+    let comment = Comment.get(commentIndex);
+    res.render('comments/form', { title: 'BookedIn || Comments', comment: comment, commentIndex: commentIndex });
 });
 
 router.post('/upsert', async (req, res, next) => {
-    console.log('body: ' + JSON.stringify(req.body))
-    let bookId = req.body.bookId;
+    console.log('body: ' + JSON.stringify(req.body));
+    let bookId = req.body.bookId; // need to put bookId in end so knows where to pull from
     redirect = `/books/show/${bookId}`;
     Comment.upsert(req.body);
+    let createdOrupdated = req.body.id ? 'updated' : 'created';
     req.session.flash = {
         type: 'info',
         intro: 'Success!',
-        message: 'Your comment has been added',
+        message: `Your comment has been ${createdOrupdated}!`,
     };
-    res.redirect(303, redirect)
+    res.redirect(303, redirect);
 });
 
 module.exports = router;
+
